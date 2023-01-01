595ef34f9a9b6837d70253eca4e4c45882cce7e1
Kitzdev <kitzdev.dev@gmail.com>
2023-01-02 01:22:51 +0700

    Add model for houses table

--- app/model/HouseModel.php
+++ app/model/HouseModel.php
@@ -1,10 +1,18 @@
 <?php
 
-namespace model;
-class House
+class HouseModel
 {
     private $table = "houses";
     private $database;
+
+    const COLUMN_TYPE = [
+        "id" => PDO::PARAM_NULL,
+        "model" => PDO::PARAM_STR,
+        "address" => PDO::PARAM_STR,
+        "price_per_month" => PDO::PARAM_STR,
+        "rented_status" => PDO::PARAM_STR,
+        "created_at" => PDO::PARAM_STR,
+    ];
 
     public function __construct()
     {
@@ -13,17 +21,22 @@ class House
 
     public function getSpecificHouse($houseId)
     {
-
+        $this->database->query("SELECT model, address, price_per_month FROM $this->table WHERE id =:id");
+        $this->database->bind('id', $houseId, PDO::PARAM_STR);
+        return $this->database->fetchSingle();
     }
 
     public function getAllHouse()
     {
-
+        $this->database->query("SELECT * FROM $this->table");
+        return $this->database->fetchAll();
     }
 
     public function getAllHouseAddress()
     {
-
+        $this->database->query("SELECT address FROM $this->table WHERE rented_status=:rented_status");
+        $this->database->bind('rented_status', 0, PDO::PARAM_INT);
+        return $this->database->fetchAll();
     }
 
     /**
@@ -38,12 +51,12 @@ class House
      */
     public function createHouse($data): mixed
     {
-        $query = "INSERT INTO {$this->table} VALUES('', :model, :address, :price_per_month, :created_at)";
+        $query = "INSERT INTO {$this->table} (model, address, price_per_month, created_at) VALUES(:model, :address, :price_per_month, :created_at)";
         $this->database->query($query);
-        $this->database->bind('model', $data['model']);
-        $this->database->bind('address', $data['address']);
-        $this->database->bind('price_per_month', $data['price_per_month']);
-        $this->database->bind('created_at', $data['created_at']);
+        $this->database->bind('model', $data['model'], self::COLUMN_TYPE['model']);
+        $this->database->bind('address', $data['address'], self::COLUMN_TYPE['address']);
+        $this->database->bind('price_per_month', $data['price_per_month'], self::COLUMN_TYPE['price_per_month']);
+        $this->database->bind('created_at', $data['created_at'], self::COLUMN_TYPE['created_at']);
         $this->database->execute();
 
         return $this->database->rowCount();
