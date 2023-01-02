9c4512b0f6e9962f45333383ebdeb3318e3e1b5e
Kitbert <kitzdev.dev@gmail.com>
2023-01-02 23:23:16 +0700

    Add new API for booking

--- app/controller/api/version_1/BookingController.php
+++ app/controller/api/version_1/BookingController.php
@@ -1,19 +1,78 @@
 <?php
 
-
 class BookingController extends APIController
 {
+    private BookingModel $bookingModel;
+
+    public function __construct()
+    {
+        require_once __DIR__ . "/../../../model/BookingModel.php";
+        $this->bookingModel = new BookingModel();
+    }
+
     function create()
     {
-        echo "Create a booking";
+        require_once __DIR__ . "/../../../model/HouseModel.php";
+        $houseModel = new HouseModel();
+
+        // Check if customer already booked a house.
+        $bookingDataByCustomerId = $this->bookingModel->getBookingByCustomerId($_POST['customer_id']);
+
+        if (!empty($bookingDataByCustomerId)) {
+            http_response_code(422);
+            return "Customer Id {$_POST['customer_id']} hanya dapat melakukan booking satu kali";
+        }
+
+        // Check if inserted address is available
+        $houseData = $houseModel->getHouseByAddress($_POST['house_address']);
+
+        if (empty($houseData)) {
+            http_response_code(422);
+            return "Data rumah dengan alamat yang dimasukkan tidak ditemukan";
+        }
+
+        // Check if house is already rented or not
+        if ($houseData['rented_status']) {
+            http_response_code(422);
+            return "Rumah dengan alamat {$houseData['address']} sudah di-booking";
+        }
+
+        $date = new DateTime();
+        $endDate = $date->add(new DateInterval("P{$_POST['duration']}M"))
+            ->format("Y-m-d H-i-s");
+
+        $data = [
+            'customer_id' => $_POST['customer_id'],
+            'house_address' => $_POST['house_address'],
+            'duration' => $_POST['duration'],
+            'house_model' => $houseData['model'],
+            'total_bill' => $_POST['duration'] * intval($houseData['price_per_month']),
+            'end_date' => $endDate,
+            'created_at' => date("Y-m-d H-i-s"),
+        ];
+
+        $bookingResult = $this->bookingModel->createBooking($data);
+
+        if ($bookingResult !== 1) {
+            return $bookingResult;
+        }
+
+        // Update house rented status
+        $updateRentedStatusResult = $houseModel->updateHouseRentedStatusbyAddress($_POST['house_address'], true);
+
+        if ($updateRentedStatusResult !== 1) {
+            return "Gagal memperbarui status rumah";
+        }
+
+        return $bookingResult;
     }
 
     function read($mode = "")
     {
         if (empty($mode)) {
-            echo "Get all booking";
+            return $this->bookingModel->getAllBooking();
         } else {
-            echo "Get specific booking";
+            return $this->bookingModel->getSpecificBooking($mode);
         }
     }
 
--- app/model/BookingModel.php
+++ app/model/BookingModel.php
@@ -33,19 +33,63 @@ class BookingModel
      * }
      * @return mixed
      */
-    public function createHouse($data): mixed
+    public function createBooking($data): mixed
     {
-        $query = "INSERT INTO {$this->table} (customer_id, house_address, duration, total_bill, end_date, created_at) 
-                    VALUES(:customer_id, :house_address, :duration, :total_bill, :end_date, :created_at)";
-        $this->database->query($query);
-        $this->database->bind('customer_id', $data['customer_id'], self::COLUMN_TYPE['customer_id']);
-        $this->database->bind('house_address', $data['house_address'], self::COLUMN_TYPE['house_address']);
-        $this->database->bind('duration', $data['duration'], self::COLUMN_TYPE['duration']);
-        $this->database->bind('total_bill', $data['total_bill'], self::COLUMN_TYPE['total_bill']);
-        $this->database->bind('end_date', $data['end_date'], self::COLUMN_TYPE['end_date']);
-        $this->database->bind('created_at', $data['created_at'], self::COLUMN_TYPE['created_at']);
-        $this->database->execute();
+        $query = "INSERT INTO {$this->table} (customer_id, house_address, house_model, duration, total_bill, end_date, created_at) 
+                    VALUES(:customer_id, :house_address, :house_model, :duration, :total_bill, :end_date, :created_at)";
+        $this->database->query($query)
+            ->bind('customer_id', $data['customer_id'], self::COLUMN_TYPE['customer_id'])
+            ->bind('house_address', $data['house_address'], self::COLUMN_TYPE['house_address'])
+            ->bind('house_model', $data['house_model'], self::COLUMN_TYPE['house_model'])
+            ->bind('duration', $data['duration'], self::COLUMN_TYPE['duration'])
+            ->bind('total_bill', $data['total_bill'], self::COLUMN_TYPE['total_bill'])
+            ->bind('end_date', $data['end_date'], self::COLUMN_TYPE['end_date'])
+            ->bind('created_at', $data['created_at'], self::COLUMN_TYPE['created_at'])
+            ->execute();
+
+        if ($this->database->isError()) {
+            http_response_code(422);
+            return $this->createBookingErrorMessage();
+        }
 
         return $this->database->rowCount();
+    }
+
+    public function createBookingErrorMessage(): string
+    {
+        $errorCode = $this->database->getErrorCode();
+
+        $message = "";
+        if (!empty($errorCode)) {
+            switch ($errorCode) {
+                case "P0001":
+                    $message = "Customer ID harus berjumlah 16 digit";
+                    break;
+                default:
+                    $message = "Terjadi kesalahan saat memasukkan data";
+            }
+        }
+
+        return $message;
+    }
+
+    public function getBookingByCustomerId($customerId)
+    {
+        $this->database->query("SELECT * FROM $this->table WHERE customer_id = :customer_id")
+            ->bind('customer_id', $customerId, PDO::PARAM_STR);
+        return $this->database->fetchSingle();
+    }
+
+    public function getAllBooking()
+    {
+        $this->database->query("SELECT customer_id, house_model, house_address, end_date FROM $this->table");
+        return $this->database->fetchAll();
+    }
+
+    public function getSpecificBooking($bookingId)
+    {
+        $this->database->query("SELECT * FROM $this->table WHERE booking_id =:booking_id");
+        $this->database->bind('booking_id', $bookingId, PDO::PARAM_STR);
+        return $this->database->fetchSingle();
     }
 }
